29d4c1567701cdb847c272a01a85ef130ab41768
TPCoRE <2192749414@qq.com>
2018-07-12 23:00:47 +0800

    first

---
+++ src/com/shincolle/nclient/Starter.java
@@ -0,0 +1,17 @@
+package com.shincolle.nclient;
+
+/**
+ * NClient Starter
+ * */
+public final class Starter {
+	
+	/**
+	 * Start nclient
+	 * */
+	public static void main(String[] args) {
+		System.out.write(0);
+		System.out.flush();
+		//TODO
+		while(true);
+	}
+}
