d76d25b4a7b3d52ae3b03ec4bce92f6b635b3722
TPCoRE <2192749414@qq.com>
2018-07-13 10:00:01 +0800

    change the starter mode

---
+++ src/com/shincolle/nclient/NClient.java
@@ -0,0 +1,16 @@
+package com.shincolle.nclient;
+
+import java.util.UUID;
+
+/**
+ * NClient
+ * */
+public final class NClient {
+	
+	/**
+	 * Launch nclient with given uuid
+	 * */
+	public NClient(UUID uuid) {
+		throw new IllegalArgumentException(uuid.toString()); //TODO
+	}
+}
